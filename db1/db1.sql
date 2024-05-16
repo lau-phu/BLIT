4d8f2b8a55d7390ba45cb6357667e80c481433b5
unknown <plau123@ymail.com>
2024-05-16 15:48:54 -0400

    database homework, db1

--- db1/db1.sql
+++ db1/db1.sql
@@ -6,7 +6,7 @@ cusEmail varchar(30)
 
 );
 create table product(
-productId int primary key,
+proId int primary key,
 proName varchar(30),
 price int
 );
@@ -21,3 +21,21 @@ foreign key (cusId) references customer(cusId),
 foreign key (proId) references product(proId)
 );
 
+insert into customer (cusId, cusName, cusEmail)
+values (1,"idiot","1@idiot.com"),
+(4,"dumby","1@dumby.com"),
+(2,"dumdum","1@dumdum.com"),
+(3,"honk","1@peepeepee.com");
+;
+insert into product (proId, proName, price)
+values (1,"apple",100000),
+(4,"pear",1234),
+(2,"pineapple",32),
+(3,"pen",123123123);
+;
+insert into orders (orderId,cusId,proId, quanity, orderDate)
+values (1,1,1,4,"december 33 2023"),
+(4,4,4,5,"feb 30 2022"),
+(2,2,2,56,"january 1 1"),
+(3,3,3,999,"june 9, 6969");
+;
